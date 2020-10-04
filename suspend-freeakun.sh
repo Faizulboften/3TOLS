12ebe7b371c85a7c3cfb215d8c3faa3383c7940f
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-04 14:39:29 +0800

    Create suspend-freeakun.sh

---
+++ suspend-freeakun.sh
@@ -0,0 +1,79 @@
+clear
+echo $cyan " 🔴 INSTALL TOOLSN "
+sleep 10
+echo $cyan " ✔ MASUK "
+
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+
+echo $bi "
+╭━┳━╭━╭━╮             🔓FAJZUL BOFTEN
+┃┈┈┈┣▅╋▅┫┃            🔓20 SCRIP TOOLS HACKIG
+┃┈┃┈╰━╰━━━━━━╮       🔓JANGAN RECODE  SCRIP GUA
+╰┳╯┈┈┈┈┈┈┈┈┈◢▉◣      🔴INDONESIA
+╲┃┈┈┈┈┈┈┈┈┈┈▉▉▉      🔴SILTENG
+╲┃┈┈┈┈┈┈┈┈┈┈◥▉◤      🔴AMPANA KOTA
+╲┃┈┈┈┈╭━┳━━━━╯╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗ 🔓 : 082271426251
+╲┣━━━━━━┫      ║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝ 🔓 : NOTE🔴
+╲┃┈┈┈┈┈┈┃      ╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩    🔓 : SULTENG 
+
+echo $or "
+     
+          ════════════════════════════════════
+                 🔓 MENU 3 TOOLS BY ZUL 🔓
+          ════════════════════════════════════ 
+          ════════════════════════════════════
+          ════════════════════════════════════
+          [[1]] 🔓 [ AKUN FREE FIRE GRATIS ]
+          ════════════════════════════════════
+          [[2]] 🔓   [ OPEN AKUN SUSPEND     ]
+          ════════════════════════════════════
+          ┠
+          ┗────[[99]] EXIT/PROGTAM🔴"
+                echo '\033[35;1m'
+                read -p "root@Pilih Nomor > " bro
+                
+               
+               if [ $bro = 1 ] || [ $bro = 1 ]
+               then
+               sleep 12
+               echo $cyan " 🔓 MENGINSTALL "
+               sleep 1
+               echo $cyan " 🔓 MASUK DAN LOGIN"
+               sleep 10
+               pkg install git
+               git clone https://github.com/Faizulboften/2TOOLSACCOND
+               cd 2TOOLSACCOND
+               sh 3acc.sh
+               fi
+               
+               if [ $bro = 2 ] || [ $bro = 2 ]
+               then
+               sleep 10
+               echo $cyan "🔓 MENGGINSTALL"
+               sleep 10
+               echo $cyan "🔓 BUKA AKUN ANDA
+               sleep 10
+               pkg install git
+               git clone https://github.com/Faizulboften/garena
+               cd garena
+               sh jos.sh
+              fi
+              
+              
+               if [ $bro = 99 ] || [ $bro = 99 ]
+               then
+               echo $cyan  "Subscribe Channel Htc Ctr Gaming "
+               sleep 1
+               exit
+               fi
+            
+          
+         
+       
